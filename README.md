9667d43710d7b3589f9a63f436013ae9ce7b38cb
sergiorobla <sergiorobla36@gmail.com>
2024-04-07 15:34:15 +0200

    version 2

---
+++ README.md
@@ -0,0 +1,10 @@
+# Digitaliza tu mundo
+
+Proyecto de Desarrollo y Consultoria Web.
+
+- Desarrollo multiplataforma
+- Consultoria Web
+- Planificación Web
+- Mantenimiento Web
+
+## Index
--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,21 @@ document.addEventListener("DOMContentLoaded", () => {
                 else if (target.closest("#heading-title")) {
                     target.classList.add("slide-in-bottom");
                 }
+                else if (target.closest("#sn-intro")) {
+                    target.classList.add("slide-in-bottom");
+                }
+                else if (target.closest("#sn-valores")) {
+                    target.classList.add("slide-in-bottom");
+                }
+                else if (target.closest("#sn-equipo")) {
+                    target.classList.add("slide-in-bottom");
+                }
+                else if (target.closest("#sn-oferta")) {
+                    target.classList.add("slide-in-bottom");
+                }
+                else if (target.closest("#sn-conecta")) {
+                    target.classList.add("slide-in-bottom");
+                }
                 observer.unobserve(target);
             }
         });
@@ -26,7 +41,49 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll("#servicios > div").forEach((el) => {
         observer.observe(el);
     });
+    // Observa todos los elementos dentro de #heading-title
     document.querySelectorAll("#heading-title > div").forEach((el) => {
+        observer.observe(el);
+    });
+    // Observa todos los elementos dentro de #sn-intro
+    document.querySelectorAll("#sn-intro > img").forEach((el) => {
+        observer.observe(el);
+    });
+    document.querySelectorAll("#sn-intro > div").forEach((el) => {
+        observer.observe(el);
+    });
+    // Observa todos los elementos dentro de #sn-valores
+    document.querySelectorAll("#sn-valores > img").forEach((el) => {
+        observer.observe(el);
+    });
+    document.querySelectorAll("#sn-valores > div h1").forEach((el) => {
+        observer.observe(el);
+    });
+    document.querySelectorAll("#sn-valores > div p").forEach((el) => {
+        observer.observe(el);
+    });
+    document.querySelectorAll("#sn-valores > div h3").forEach((el) => {
+        observer.observe(el);
+    });
+    // Observa todos los elementos dentro de #sn-equipo
+    document.querySelectorAll("#sn-equipo > img").forEach((el) => {
+        observer.observe(el);
+    });
+    document.querySelectorAll("#sn-equipo > div").forEach((el) => {
+        observer.observe(el);
+    });
+    // Observa todos los elementos dentro de #sn-oferta
+    document.querySelectorAll("#sn-oferta > img").forEach((el) => {
+        observer.observe(el);
+    });
+    document.querySelectorAll("#sn-oferta > div").forEach((el) => {
+        observer.observe(el);
+    });
+    // Observa todos los elementos dentro de #sn-conecta
+    document.querySelectorAll("#sn-conecta > img").forEach((el) => {
+        observer.observe(el);
+    });
+    document.querySelectorAll("#sn-conecta > div").forEach((el) => {
         observer.observe(el);
     });
 });
